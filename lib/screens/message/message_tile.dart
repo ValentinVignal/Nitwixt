52fd21ad28f61bd8f0bb6021d1477c1bbd6a8e56
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-05-11 04:28:15 +0800

    Add the time

--- lib/screens/message/message_tile.dart
+++ lib/screens/message/message_tile.dart
@@ -1,11 +1,16 @@
+import 'package:dash_chat/dash_chat.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:textwit/models/models.dart' as models;
 
 class MessageTile extends StatefulWidget {
   models.Message message;
+  DateFormat format = DateFormat('HH:mm');
+  DateTime date;
 
-  MessageTile({this.message});
+  MessageTile({this.message}) {
+    date = DateTime.fromMillisecondsSinceEpoch(message.date.millisecondsSinceEpoch * 1000);
+  }
 
   @override
   _MessageTileState createState() => _MessageTileState();
@@ -18,25 +23,62 @@ class _MessageTileState extends State<MessageTile> {
 
     bool isMyMessage = user.id == widget.message.userid;
 
+    Widget dateContainer = Container(
+      width: 45.0,
+      child: Padding(
+        padding: const EdgeInsets.all(0.0),
+        child: Text(widget.format.format(widget.date).toString(),),
+      ),
+      padding: EdgeInsets.only(
+        left: isMyMessage ? 6.0 : 0.4,
+        right: isMyMessage ? 0.4 : 6.0,
+        top: 10.0,
+        bottom: 0.0,
+      ),
+      alignment: isMyMessage ? Alignment.bottomRight : Alignment.bottomLeft,
+      );
+
     return Row(
-      mainAxisAlignment: isMyMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: <Widget>[
         Flexible(
-          child: Padding(
-            padding: isMyMessage ? EdgeInsets.only(left: 40.0) : EdgeInsets.only(right: 40.0),
-            child: Container(
-              margin: EdgeInsets.all(2.0),
-              padding: EdgeInsets.only(top: 7.0, bottom: 10.0, right: 8.0, left: 8.0),
-              decoration: BoxDecoration(
-                color: isMyMessage ? Colors.blue[400] : Colors.black,
-                borderRadius: BorderRadius.all(Radius.circular(20.0,),),
+          child: Row(
+            mainAxisAlignment: isMyMessage ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: <Widget>[
+              isMyMessage
+                  ? dateContainer
+                  : Container(
+                      height: 0.0,
+                      width: 0.0,
+                    ),
+              Flexible(
+                child: Container(
+                  margin: EdgeInsets.all(2.0),
+                  padding: EdgeInsets.only(top: 7.0, bottom: 10.0, right: 8.0, left: 8.0),
+                  decoration: BoxDecoration(
+                    color: isMyMessage ? Colors.blue[400] : Colors.black,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        20.0,
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    widget.message.text,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ),
               ),
-              child: Text(
-                widget.message.text,
-                style: TextStyle(color: Colors.white, fontSize: 20.0,),
-                textAlign: TextAlign.justify,
-              ),
-            ),
+              !isMyMessage
+                  ? dateContainer
+                  : Container(
+                      height: 0.0,
+                      width: 0.0,
+                    ),
+            ],
           ),
         )
       ],
